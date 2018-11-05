baae52dfccd0518d83d9c2ef32694a78d3fdf758
juan de dios canche cen <juan1910@live.com.mx>
2018-11-05 14:29:25 -0600

    initial commit

---
+++ lightbox-ejemplo/assets/js/app.js
@@ -0,0 +1,25 @@
+const app = new Vue({
+   el: "#app",
+   data: {
+      gatos: [
+         'assets/images/gato1.jpg',
+         'assets/images/gato2.jpg',
+         'assets/images/gato3.jpg',
+         'assets/images/gato4.jpg',
+         'assets/images/gato5.jpg',
+         'assets/images/gato6.jpg',
+         'assets/images/gato7.jpg',
+         'assets/images/gato8.jpg',
+         'assets/images/gato9.jpg',
+         'assets/images/gato10.jpg',
+         'assets/images/gato11.jpg',
+         'assets/images/gato12.jpg'
+      ]
+   },
+   methods: {
+
+   },
+   created () {
+
+   }
+});
